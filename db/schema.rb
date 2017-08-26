2cd86056b2a8bf204033bae11cb0701ada8933d9
Joan Sebastian Contreras <jscontrerasp@unal.edu.co>
2017-08-26 15:57:31 +0000

    añadiendo 3r modelo city

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170826132714) do
+ActiveRecord::Schema.define(version: 20170826144359) do
 
   create_table "cars", force: :cascade do |t|
     t.string "color"
     t.string "type_car"
     t.float "weight"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.integer "population"
+    t.integer "buildings"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
